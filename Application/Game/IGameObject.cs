ad769bb09edbf3a879dec877c6004177b6468315
XaDanX <Listek05@gmail.com>
2022-07-03 17:32:45 +0200

    added GameObject interface and SpellBook structures.

---
+++ Application/Game/IGameObject.cs
@@ -0,0 +1,26 @@
+﻿namespace Application.Game;
+
+//rozdziel se to jakos
+
+
+public interface IGameObject
+{
+    public IntPtr BaseAddress { get; }
+    public float Health { get; }
+    public float MaxHealth { get; }
+    public int Team { get; }
+    public RiotString Name { get; } // [(IntPtr)NamePtr] + 0x00 (50 length)
+    public bool IsAlive { get; }
+    public bool IsVisible { get; }
+    public float AttackRange { get; }
+    public float Armor { get; }
+    public float AttackDamage { get; } // base + bonus attack dmg (two addresses)
+    public float CriticalChance { get; }
+    public Vector3 Position { get; }
+    public float MovementSpeed { get; }
+    public bool IsMoving { get; }
+    public bool AttackSpeedMultiplier { get; } // multiplier (base_attack_speed * multiplier = attack speed)
+
+    public SpellBook GetSpellBook { get; }
+
+}
---
+++ Application/Game/ISpell.cs
@@ -0,0 +1,53 @@
+﻿namespace Application.Game;
+
+public struct SpellSlot
+{
+    public SpellSlot(float readyTime, float level, String name, float damage, float readyAt)
+    {
+        ReadyTime = readyTime;
+        Level = level;
+        Name = name;
+        Damage = damage;
+        ReadyAt = readyAt;
+    }
+    
+    public float ReadyTime { get; }
+    public float ReadyAt { get; }
+
+    public float Damage { get; }
+
+    public string Name { get; }
+
+    public float Level { get; }
+
+    
+}
+
+public struct SpellBook
+{
+    public SpellBook(int objectSpellBookAddress)
+    {
+        objectSpellBookAddress = objectSpellBookAddress;
+        SpellQ = default;
+        SpellW = default;
+        SpellE = default;
+        SpellR = default;
+        SpellD = default;
+        SpellF = default;
+    }
+    
+    public SpellSlot SpellQ { get; set; }
+    public SpellSlot SpellW { get; set; }
+    public SpellSlot SpellE { get; set; }
+    public SpellSlot SpellR { get; set; }
+    public SpellSlot SpellD { get; set; }
+    public SpellSlot SpellF { get; set; }
+
+    SpellBook Load()
+    {
+        // ladowanie spell booka i slotow z pamieci
+        // [[<league of legends.exe> + ObjectPointer] + SPELL_BOOK_ADDRESS + 0x488 + (0x4*0)] // 0x488 = spell slot (0x4*i) i= index
+        return this;
+    }
+    
+}
