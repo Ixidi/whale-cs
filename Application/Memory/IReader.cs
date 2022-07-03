b62e5466b2f34ee3968ca8416828541f64eb2b75
Ixidi <xlinux3@gmail.com>
2022-07-03 20:12:53 +0200

    memory reader

---
+++ Application/Memory/IReader.cs
@@ -0,0 +1,17 @@
+﻿using Application.Math;
+
+namespace Application.Memory;
+
+public interface IReader
+{
+
+    string ReadString(int address);
+    float ReadFloat(int address);
+    int ReadInt(int address);
+    short ReadShort(int address);
+    byte ReadByte(int address);
+    IVector3 ReadVector3(int address);
+
+    T Read<T>(int address);
+
+}
---
+++ Application/Memory/Offsets.cs
@@ -0,0 +1,23 @@
+﻿namespace Application.Game;
+
+public static class Offsets
+{
+    public static int OIndex = 0x8;
+    public static int OTeam = 0x34;
+    public static int OHealth = 0xE74;
+    public static int OMaxHealth = 0xE84;
+    public static int OPosition = 0x1DC;
+    public static int OIsVisible = 0x274;
+    public static int OMana = 0x29C;
+    public static int OName = 0x2BA4;
+    public static int OSpawnCount = 0x288;
+    public static int OAttackRange = 0x12B4;
+    public static int OMagicResist = 0x1380; //might be wrong
+    public static int OAttackDamage = 0x134C;
+    public static int OBonusAttackDamage = 0x12C4;
+    public static int OCritChance = 0x12C8;
+    public static int OIsMoving = 0x32E7;
+    public static int OAbilityPower = 0x11F4;
+    public static int OAbilityPowerMultiplier = 0x11F8;
+    public static int ONetworkId = 0xB4;
+}
